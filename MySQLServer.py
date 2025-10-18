46ec8d00c8d331ec41c891b0ef8daf860a979d73
Emmanuel <emmanuelizu92@gmail.com>
2025-10-18 13:58:43 -0700

    Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,15 +1,18 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = '@1Awspolicyss'
-)
+try:
+    mydb = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = '@1Awspolicyss'
+    )
 
-if not mydb.is_connected:
-    print('Error while connecting to Mysql')
+    if not mydb.is_connected:
+        print('Error while connecting to Mysql')
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-mycursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
-print("Database 'alx_book_store' created successfully!")
+    mycursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
+    print("Database 'alx_book_store' created successfully!")
+except mysql.connector.errors as err:
+    print(f'Error {err} Encountered')
