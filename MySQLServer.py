8f4409cbb5a5aeb4d19d50e3dce0a308f40f0818
Emmanuel <emmanuelizu92@gmail.com>
2025-10-18 14:00:34 -0700

    Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -14,5 +14,5 @@ try:
 
     mycursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
     print("Database 'alx_book_store' created successfully!")
-except mysql.connector.errors as err:
+except mysql.connector.Error as err:
     print(f'Error {err} Encountered')
